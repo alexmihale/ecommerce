0a4524a06a78bc4baf560b0b2efeaec4fb69a1a8
Andrei Mihale <andrei.mihale97@gmail.com>
2020-12-29 18:22:47 +0200

    - added extensions for project - changed FIXME into REVIEW

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -17,7 +17,7 @@ const productSchema = new mongoose.Schema(
     //  Specs will be saved as an object
     // to be able to create dynamic specification table
     specs: { type: Object, required: true },
-    //  FIXME: the variant type should be as a string or as below?
+    //  REVIEW: the variant type should be as a string or as below?
     //  variants are same products but different specs ( like memmory size, color etc.)
     //  variant should store id of product
     variant: [{ type: mongoose.Schema.Types.ObjectId, default: null }],
